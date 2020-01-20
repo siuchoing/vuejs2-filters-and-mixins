4617d6fcd92731107004247267d59155b600545e
altantic <15200582@life.hlkbu.edu.hk>
2020-01-20 23:51:07 +0800

    move friut list to left side

--- src/List.vue
+++ src/List.vue
@@ -1,14 +1,10 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Filters & Mixins</h1>
-                <input v-model="filterText">
-                <ul>
-                    <li v-for="fruit in filteredFruits">{{ fruit }}</li>
-                </ul>
-            </div>
-        </div>
+    <div>
+        <h1>Filters & Mixins</h1>
+        <input v-model="filterText">
+        <ul>
+            <li v-for="fruit in filteredFruits">{{ fruit }}</li>
+        </ul>
     </div>
 </template>
 
