925827bc9ea6d48cfa7b4f879d198730cdf77287
altantic <15200582@life.hlkbu.edu.hk>
2020-01-20 23:47:33 +0800

    Create List component

--- src/List.vue
+++ src/List.vue
@@ -3,33 +3,21 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filters & Mixins</h1>
-                <p>{{ text | toUppercase | to-lowercase }}</p>
-                <hr>
                 <input v-model="filterText">
                 <ul>
                     <li v-for="fruit in filteredFruits">{{ fruit }}</li>
                 </ul>
-                <hr>
-                <app-list></app-list>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import List from './List.vue';
-
     export default {
         data() {
             return {
-                text: 'Hello there!',
                 fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
                 filterText: ''
-            }
-        },
-        filters: {
-            toUppercase(value) {
-                return value.toUpperCase();
             }
         },
         computed: {
@@ -39,9 +27,6 @@
                     return element.match(this.filterText);
                 });
             }
-        },
-        components: {
-            appList: List
         }
     }
 </script>
