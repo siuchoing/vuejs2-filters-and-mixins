43fdb82dee9e827a15dd50f019cc9520a6eb7d2d
altantic <15200582@life.hlkbu.edu.hk>
2020-01-21 00:09:58 +0800

    using fruitMixin in List component

--- src/List.vue
+++ src/List.vue
@@ -9,21 +9,10 @@
 </template>
 
 <script>
+    import { fruitMixin } from './fruitMixin';
+
     export default {
-        data() {
-            return {
-                fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
-                filterText: ''
-            }
-        },
-        computed: {
-            filteredFruits() {
-                /* in this (element) callback,will return all them elements I generated*/
-                return this.fruits.filter((element) => {
-                    return element.match(this.filterText);
-                });
-            }
-        }
+        mixins: [fruitMixin]
     }
 </script>
 
