bc010bbc3834ceb61bea9dba9784b173cf75a0b5
altantic <15200582@life.hlkbu.edu.hk>
2020-01-21 22:27:06 +0800

    passing value by data properties

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,17 @@
                 <h1>Filters & Mixins</h1>
                 <!-- Exercise 1) -->
                 <!-- Build a local Filter which reverses the Text it is applied on -->
-                <p>{{ 'Some Text' | reverse }}</p>
+                <p>{{ firstText | reverse }}</p>
 
                 <!-- Exercise 2 -->
                 <!-- Build a global Filter which counts the length of a word and it appends it -->
                 <!-- Like this: "Test" => Gets Filtered to => "Test (4)" -->
-                <p>{{ 'Text' | calculateLength }}</p>
+                <p>{{ secondText | calculateLength }}</p>
 
                 <!-- Exercise 3 -->
                 <!-- Do the same as in Exercises 1 & 2, now with Computed Properties -->
+                <p>{{ firstText | reverse }}</p>
+                <p>{{ secondText | calculateLength }}</p>
 
                 <!-- Exercise 4 -->
                 <!-- Share the Computed Property rebuilding Exercise 2 via a Mixin -->
@@ -24,6 +26,12 @@
 
 <script>
     export default {
+        data() {
+            return {
+                firstText: 'Some Text',
+                secondText: 'Maximilian'
+            }
+        },
         filters: {
             reverse(value) {
                 return value.split("").reverse().join("");
